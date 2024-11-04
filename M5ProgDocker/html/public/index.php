57f5242c52bf14e77649e1ebf859b1e57ae7bfaf
Robin Lemmers <38210@ma-web.nl>
2024-11-04 12:47:50 +0100

    data ophalen uit de nieuwe book tabel

--- M5ProgDocker/html/public/index.php
+++ M5ProgDocker/html/public/index.php
@@ -5,7 +5,6 @@ include_once("../source/database.php");
 
 $connection = database_connect();
 
-$result = $connection->query("SELECT 'het werkt' as nice");
+$result = $connection->query("SELECT * from book");
 
 print_r($result->fetch_all());
-
